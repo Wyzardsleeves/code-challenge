ea6c964b992aa1837a4bf4b67b8675a0217f3c42
Justin Lattimore <jlattimoreweb@gmail.com>
2020-07-15 23:05:51 -0500

    parser function working

--- coding-challenge.rb
+++ coding-challenge.rb
@@ -1,6 +1,13 @@
 require 'nokogiri'
 
-html = File.open("files/van-gogh-paintings.html")
-parsed_data = Nokogiri::HTML.parse(html)
+class FileParse
+  def start
+    html = File.open("files/van-gogh-paintings.html")
+    parsed_data = Nokogiri::HTML.parse(html)
 
-puts parsed_data.title
+    puts parsed_data.title
+  end
+end
+
+fileParse = FileParse.new
+fileParse.start
