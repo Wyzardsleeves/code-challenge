f99b5240a218430d6c5ed444f97f9384ca621616
Justin Lattimore <jlattimoreweb@gmail.com>
2020-07-17 16:47:54 -0500

    completed program

--- coding-challenge.rb
+++ coding-challenge.rb
@@ -2,10 +2,30 @@ require 'nokogiri'
 
 class FileParse
   def start
-    html = File.open("files/van-gogh-paintings.html")
-    parsed_data = Nokogiri::HTML.parse(html)
+    array = []
 
-    puts parsed_data.title
+    #get parsed data
+    html = File.open("files/van-gogh-paintings.html")
+    parsed_data = Nokogiri::HTML.parse(html).css(".mlo-c")
+
+    #asort into array
+    parsed_data.each do |item|
+      array.push({
+        name: item.css(".kltat").css("span").text,
+        extensions: ext_arr(item.css(".ellip")),
+        link: item.at_css(".klitem")["href"],
+        image: item.at_css("img")["data-src"]
+      })
+    end
+
+    puts array
+  end
+
+  #helper method for parsing multiple extensions in case another is found with the same className
+  def ext_arr(ext)
+    temp_arr = []
+    ext.each{|e| temp_arr.push(e.text) }
+    return temp_arr
   end
 end
 
